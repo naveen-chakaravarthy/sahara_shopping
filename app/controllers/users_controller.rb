5fe55ef0dec5db8b63c64872dfcc263fd76c4240
naveen-chakaravarthy <naveen.chakaravarthy@freshworks.com>
2022-09-28 14:21:46 +0530

    Signup and Signin modules completed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def new
+    render "new"
+  end
+
+  def create
+    user = User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+    )
+
+    flash[:info] = "Customer Signup Successful - #{user.id}. #{user.email}"
+    redirect_to new_session_path
+  end
+end
