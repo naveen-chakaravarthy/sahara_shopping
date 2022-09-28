2095a86f8819a44cc17eb902b00664ea447c45f6
naveen-chakaravarthy <naveen.chakaravarthy@freshworks.com>
2022-09-28 17:23:24 +0530

    New route added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/" => "application#index"
 
-  resources :sessions, :users
+  resources :sessions, :users, :shop
 end
