2a084ec9989e0e38253355458b31c6f0c9cb093d
naveen-chakaravarthy <naveen.chakaravarthy@freshworks.com>
2022-10-26 16:56:47 +0530

    Update app/controllers/order_item_controller.rb
    Co-authored-by: Ramya Vardhan <ramya.vardhan@freshworks.com>

--- app/controllers/order_item_controller.rb
+++ app/controllers/order_item_controller.rb
@@ -1,7 +1,6 @@
 class OrderItemController < ApplicationController
   def index
-    order_id = params[:order_id]
-    render "index", locals: {order_id: order_id}
+    @order_id =  params[:order_id]
   end
 
 end
